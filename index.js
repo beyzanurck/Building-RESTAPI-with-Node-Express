1996bdfa88fca6812cc4635475ccdcd54c99edd2
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-23 14:16:25 -0500

    imports books to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import BOOKS from "./books";
 
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(cors());
 //Middleware - pwd
 const _dirname = path.resolve();
 
+//Defines an API endpoint
 app.get('/', (req, res) => {
     console.log('home page');
     res.send("This is Beyza");
