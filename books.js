f8f3c90977f7eb8e08524029bb9192a5b09bd53f
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-23 14:49:27 -0500

    changes the info of books.js

--- books.js
+++ books.js
@@ -1,68 +1,38 @@
 const BOOKS = [{
-    "isbn": "0553120441",
-    "title": "Harry Potter and the Sorcerer's Stone",
-    "author": "J.K. Rowling",
+    "ID": "1",
+    "title": "Deep Work",
+    "author": "Cal Newport",
     "format": "Paperback"
 },
 {
-    "isbn": "0679889175",
-    "title": "Harry Potter and the Chamber of Secrets",
-    "author": "J.K. Rowling",
+    "ID": "2",
+    "title": "Digital Minimalism",
+    "author": "Cal Newport",
     "format": "Paperback"
 },
 {
-    "isbn": "9780857660282",
-    "title": "Harry Potter and the Prisoner of Azkaban",
-    "author": "J.K. Rowling",
+    "ID": "3",
+    "title": "What I Talk About When I Talk About Running",
+    "author": "Haruki Murakami",
     "format": "Paperback"
 },
 {
-    "isbn": "0553096095",
-    "title": "Harry Potter and the Goblet of Fire",
-    "author": "J.K. Rowling",
-    "format": "Hardcover"
+    "ID": "4",
+    "title": "The Education Of The Will",
+    "author": "Jules Payot",
+    "format": "Paperback"
 },
 {
-    "isbn": "05530962356",
-    "title": "Harry Potter and the Order of the Phoenix",
-    "author": "J.K. Rowling",
-    "format": "Hardcover"
+    "ID": "5",
+    "title": "Atomic Habits",
+    "author": "James Clear",
+    "format": "Paperback"
 },
 {
-    "isbn": "0553098976",
-    "title": "Harry Potter and the Half-Blood Prince",
-    "author": "J.K. Rowling",
-    "format": "Hardcover"
-},
-{
-    "isbn": "0553093476",
-    "title": "Harry Potter and the Deathly Hallows",
-    "author": "J.K. Rowling",
-    "format": "Hardcover"
-},
-{
-    "isbn": "0553115634",
-    "title": "A Court of Thorns and Roses",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
-},
-{
-    "isbn": "0553115765",
-    "title": "A Court of Mist and Fury",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
-},
-{
-    "isbn": "05531324567",
-    "title": "A Court of Wings and Ruin",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
-},
-{
-    "isbn": "05531324511",
-    "title": "A Court of Silver Flames",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
+    "ID": "6",
+    "title": "Educated",
+    "author": "Tara Westover",
+    "format": "Paperback"
 }
 ]
 
